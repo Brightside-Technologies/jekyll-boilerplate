48731a1caa71648e52a7a621241c7d9a202ed8d4
Diego Bernal <diego91bernal@gmail.com>
2018-04-22 16:08:09 -0700

    refactor: add parcel as a bundler
    * change original jekyll file structure
    * add parcel build command to be run in series with jekyll build command
    * add bootstrap

---
+++ src/vendors/vendors.js
@@ -0,0 +1,8 @@
+//import "./vendors.scss";
+import "../../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../../node_modules/bootstrap/dist/css/bootstrap-grid.css";
+import "../../node_modules/bootstrap/dist/css/bootstrap-reboot.css";
+
+const $ = require("jquery");
+const popperjs = require("popper.js");
+const bootstrap = require("bootstrap");
