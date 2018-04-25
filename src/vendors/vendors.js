1329b9f4720ff7efda2fa3fb53791ac739673457
Diego Bernal <diego91bernal@gmail.com>
2018-04-24 22:07:16 -0700

    refactor(browserify): replace parcel with browserify

--- src/vendors/vendors.js
+++ src/vendors/vendors.js
@@ -1,7 +1,4 @@
-//import "./vendors.scss";
-import "../../node_modules/bootstrap/dist/css/bootstrap.css";
-import "../../node_modules/bootstrap/dist/css/bootstrap-grid.css";
-import "../../node_modules/bootstrap/dist/css/bootstrap-reboot.css";
-
-window.$ = require("jquery");
-const bootstrap = require("bootstrap/dist/js/bootstrap.bundle.js");
+require("./vendors.css");
+window.jQuery = $ = require("jquery");
+require("popper.js");
+require("bootstrap");
