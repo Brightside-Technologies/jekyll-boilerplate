73988984c8962529a9b9ca5cab77ef8ff3360f31
Diego Bernal <diego91bernal@gmail.com>
2018-09-20 16:54:31 -0700

    Update site.js

--- root/src/site.js
+++ root/src/site.js
@@ -4,7 +4,11 @@ window.$ = $;
 window.jQuery = jQuery;
 require("popper.js");
 import "bootstrap";
+
 import greet from "./greet";
+
+const responsiveImage = require("./assets/images/Sphynx.jpg?sizes[]=500,sizes[]=750,sizes[]=1000");
+console.log("responsiveImage", responsiveImage);
 
 const greetingMessage = greet("World");
 console.log(greetingMessage);
